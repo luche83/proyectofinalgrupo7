8a545b2ba53c35f27db2d8d376881d77acc6b06a
Fenixbvh683 <manidelabk031@gmail.com>
2023-07-18 09:17:49 -0300

    Modificacion de Root

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const PORT = 3030;
 
 app.use(express.static('public'));
 
-app.get('/', (req,res) => res.sendFile(path.join(__diename, 'views', 'home.html')));
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
+app.get('/productCart', (req,res) => res.sendFile(path.join(__dirname, 'views', 'productCart.html')));
+app.get('/productDetail', (req,res) => res.sendFile(path.join(__dirname, 'views', 'productDetail.html')));
+app.get('/register', (req,res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
+app.get('/login', (req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
+app.get('/footer', (req,res) => res.sendFile(path.join(__dirname, 'views', 'partial','footer.html')));
+app.get('/header', (req,res) => res.sendFile(path.join(__dirname, 'views','partial', 'header.html')));
 
-app.listem(PORT, () => console.log('Servidor Corriendo en http://localhost:'+PORT));
+app.listen(PORT, () => console.log('Servidor Corriendo en http://localhost:'+PORT));
