8c47afa32d2eda8be685e51d127b8971d99b47a8
Fenixbvh683 <manidelabk031@gmail.com>
2023-08-27 14:02:13 -0300

    Delete

--- src/controllers/products/remove.js
+++ src/controllers/products/remove.js
@@ -1,10 +1,28 @@
 const { readJSON, writeJSON } = require('../../data');
-const products = readJSON('products.json')
+const { unlinkSync, existsSync } = require("fs");
+
 
 module.exports = (req,res) => {    
-    const productModify = products.filter(product => product.id !== req.params.id)
     
-    writeJSON(productModify, 'products.json')
+    /*const productsModify = products.filter(product => product.id !== req.params.id);*/
+      
+    const products = readJSON('products.json')
 
+    const productsDelete = products.filter((product) => {
+        if (product.id === req.params.id) { 
+
+            existsSync(`./src/public/images/productos/${product.image}`)&&
+            unlinkSync(`./src/public/images/productos/${product.image}`);
+
+            /*product.name = req.body.name;
+            product.image = req.file ? req.file.filename : product.image;*/
+        }
+        /*return product;  */
+        return product.id !== req.params.id;
+        
+    });
+
+    writeJSON(productsDelete, 'products.json')
+    
     return res.redirect('/admin')
 }
