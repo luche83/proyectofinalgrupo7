330451cbf6bef85d5f42792ca1f13741ffec6a49
TomasReyes21 <tomasreyes2107gmail.com>
2023-08-29 18:59:58 -0300

    edit

--- src/controllers/editProductsController.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    
-    add : require('./editProducts/add'),
-    create : require('./editProducts/create'),
-    remove : require('./editProducts/remove'),
-    edit : require('./editProducts/edit'),
-          
-      
- }
--- src/controllers/products/update.js
+++ src/controllers/products/update.js
@@ -3,34 +3,31 @@ const { readJSON, writeJSON } = require('../../data');
 
 module.exports = (req, res) => {
 
-    const products = readJSON('products.json');
+    
     /* const categories = readJSON('categories.json');
     const regiones = readJSON('regiones.json');
     const characters = readJSON('characters.json'); */
-
-    const {title, price, discount, description, category, character, region, cant, cantMin} = req.body;
     
+    const products = readJSON('products.json');
+
     const productsModify = products.map(product => {
+        if(product.id === req.params.id){
 
-        if (product.id === req.params.id){
-
-            product.title = title.trim(),
-            product.category = category.trim(),
-            product.character = character,
-            product.region = region,
-            product.price = +price,
-            product.discount = +discount,
-            product.description = description.trim(),
-            product.cant = cant,
-            product.cantMin = cantMin
-            
+            product.title = req.body.title
+            product.category = req.body.category
+            product.character = req.body.character
+            product.region = req.body.region
+            product.price = req.body.price
+            product.discount = req.body.discount
+            product.description = req.body.description
+            product.cant = req.body.cant
+            product.cantMin = req.body.cantMin           
         }
 
         return product
     })
-
-   
+  
 		writeJSON(productsModify, 'products.json')
 
-		return res.redirect("/admin")
+		return res.redirect('/admin')
  }
