7746499fc23eaf61e3fc339f203bb2f75c4ddb41
Fenixbvh683 <manidelabk031@gmail.com>
2023-10-23 17:27:14 -0300

    Editar

--- src/controllers/products/update.js
+++ src/controllers/products/update.js
@@ -52,8 +52,8 @@ module.exports = (req, res) => {
             product.images
               .filter((image) => !image.main)
               .forEach((image) => {
-                existsSync(`./public/images/productos/${image.file}`) &&
-                  unlinkSync(`./public/images/productos/${image.file}`)
+                existsSync(`./src/public/images/productos/${image.file}`) &&
+                  unlinkSync(`./src/public/images/productos/${image.file}`)
               });
 
             db.Image.destroy({
